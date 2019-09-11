c5d4ba2728bae2b65404e23dbe846c6a9a06a41e
donnyllionaire <collinstheprogreammer@gmail.com>
2019-09-11 16:26:11 +0300

    added routes for update and delet functions

--- Nodejs/controllers/employeeController.js
+++ Nodejs/controllers/employeeController.js
@@ -20,9 +20,10 @@ router.get('/', (req, res)=>{
 
 //get request to get users with specific _Id
 router.get ('/:id', (req, res)=>{
-if (!ObjectId.isValid(req.params.id))
+if (!ObjectId.isValid(req.params.id))// checking for the validity of the ID
 return  res.status(400).send(`No record with that id: ${req.prams.id}`)
 
+ //else if the id is valid
 Employee.findById(req.params.id, (err, doc)=>{
   if(!err) {res.send(doc)}
   else{console.log('Error Retriveing employee data')}
@@ -45,6 +46,52 @@ router.post('/', (req, res)=>{
 
     })
 })
+
+
+/*Adding a router for update method*/
+router.put('/:id', (req, res)=>{
+  if(!ObjectId.isValid(req.params.id))
+  return  res.status(400).send(`No record with that id: ${req.prams.id}`)
+//if the id is valid, we go on with the update operation
+//by starting with emp Object
+var emp = {
+  name: req.body.name,
+  position: req.body.position,
+  office:req.body.office,
+  salary:req.body.salary,
+  }
+  /*
+  calling the findByIdAndUpdate() method using Employee method
+  then parse the id we have recieved from the parameter in put()
+  -{$set:emp} => tells mongodb the employee with teh parse id has to updated
+                  with the new information inside emp object
+
+  */
+  Employee.findByIdAndUpdate(res.params.id, {$set:emp}, {new:true},(err, doc)=>{
+        //checking for errors
+      if (!err){res.send(doc)}
+        else{console.log('Error updating new employee')}
+      })
+})
+
+/*A new Router for Delete function*/
+router.delete('/id', (req, res)=>{
+  if(!ObjectId.isValid(req.params.id))
+    return  res.status(400).send(`No record with that id: ${req.prams.id}`)
+  Employee.findByIdAndRemove(res.params.id,(err, doc)=>{
+    if (!err){res.send(doc)}
+      else{console.log('Error updating new employee')}  
+
+  })
+
+})
+
+
+
+
+
+
+
 
 
 
